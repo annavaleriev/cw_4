317bb2587a91ef35c624fbaefb15fe7cd2d54824
annavaleriev <filenko.a@gmail.com>
2023-12-08 21:13:51 +0300

    Файл с путями и ключами

---
+++ settings.py
@@ -0,0 +1,5 @@
+
+URL_HH = "https://api.hh.ru/vacancies"
+URL_SJ = "https://api.superjob.ru/2.0/vacancies/"
+SJ_KEY = "v3.r.120557415.86a075d39b9848d9551f5c98fd4240e508c6aac2.5268e46c167ed379c6910dad80d9d8557312dab9"
+# SJ_KEY_ = "X-Api-App-Id:SJ_KEY"
