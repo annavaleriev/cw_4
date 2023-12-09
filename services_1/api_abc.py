065c072dd6cf657eab1111dbfcadd83baae06bd3
annavaleriev <filenko.a@gmail.com>
2023-12-09 22:30:00 +0300

    Создан абстрактный класс для работы с API

---
+++ services_1/api_abc.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+
+class API(ABC):
+    """
+    Абстроктный класс для работы с API
+    """
+
+    @abstractmethod
+    def get_vacancy(self):
+        """
+        Абстрактный метод для получения вакансий по API
+        :return:
+        """
+        pass
+
+    def add_vacancy(self):
+        """
+        Абстрактный метод для добавления вакансий
+        :return:
+        """
+        pass
+
+    def select_vacancy(self):
+        """
+        Абстрактный метод для выбора вакансий
+        :return:
+        """
+        pass
