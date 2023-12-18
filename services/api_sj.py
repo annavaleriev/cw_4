7d19e91f0c87a09db0b1524f31c6b4e834e90e2f
annavaleriev <filenko.a@gmail.com>
2023-12-18 21:58:17 +0300

    Прописаны type hints для абстрактного класса SuperJobAPI(API) и docstring к методам get_response_by_page и get_all_vacancies

--- services/api_sj.py
+++ services/api_sj.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import requests
 
 from services.api_abc import API
@@ -10,32 +12,36 @@ class SuperJobAPI(API):
     """
 
     def __init__(self, keyword: str):
-        self.__keyword = keyword
+        self.__keyword: str = keyword
 
     @property
     def url(self):
         return URL_SJ
 
-    def get_response_by_page(self, page=0):
-        headers = {"X-Api-App-Id": SJ_API_KEY}  # method_name
-        params = {
+    def get_response_by_page(self, page: int = 0) -> dict[str, Any]:
+        """
+        Метод для получения вакансия с сайта SuperJob
+        :param page: номер страницы для получения данных
+        :return: словарь со списком вакансий в формате json
+        """
+        headers: dict = {"X-Api-App-Id": SJ_API_KEY}  # method_name
+        params: dict[str, Any] = {
             "keyword": self.__keyword,
             "count": COUNT_VACANCIES_BY_PAGE,
             "page": page,
             "town": 4
-
         }
         return requests.get(self.url, params, headers=headers).json()
 
-    def get_all_vacancies(self):
+    def get_all_vacancies(self) -> list[dict]:
         """
         Метод, для получения списка вакансий по нужным критериям
         :return: список со словарями по всем найденным вакансиям
         """
         all_vacancies: list = []
-        response = self.get_response_by_page()
+        response: dict[str, Any] = self.get_response_by_page()
         all_vacancies.extend(response["objects"])
-        page = 1
+        page: int = 1
         while response["more"]:
             response = self.get_response_by_page(page)
             all_vacancies.extend(response["objects"])
