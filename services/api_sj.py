8d0621d27880b249961886acc49b0f7b412663f5
annavaleriev <filenko.a@gmail.com>
2023-12-18 22:43:28 +0300

    Оптимизирован метод   def get_all_vacancies в классе SuperJobAPI(API)

--- services/api_sj.py
+++ services/api_sj.py
@@ -33,18 +33,36 @@ class SuperJobAPI(API):
         }
         return requests.get(self.url, params, headers=headers).json()
 
+    # def get_all_vacancies(self) -> list[dict]:
+    #     """
+    #     Метод, для получения списка вакансий по нужным критериям
+    #     :return: список со словарями по всем найденным вакансиям
+    #     """
+    #     all_vacancies: list = []  # создаем пустой список, туда запишем потом словари с вакансиями
+    #     response: dict[str, Any] = self.get_response_by_page() # получаем вакансии с сатй с указанной страницы,
+    #     в нашем случае с 0
+    #     all_vacancies.extend(response["objects"]) # добавляет найденные словари( 1 словарь = 1 вакансия) в список
+    #     # с вакансиями. Всего 100 шт с одной страницы
+    #     # all_vacancies.extend(self.get_response_by_page()["objects"])
+    #     page: int = 1 # принудительно делаем страницу 1, чтобы с неё пошел отсчет дальше
+    #     while response["more"]:
+    #         response = self.get_response_by_page(page)
+    #         all_vacancies.extend(response["objects"])
+    #         page += 1
+    #     return all_vacancies
+
     def get_all_vacancies(self) -> list[dict]:
         """
         Метод, для получения списка вакансий по нужным критериям
         :return: список со словарями по всем найденным вакансиям
         """
         all_vacancies: list = []
-        response: dict[str, Any] = self.get_response_by_page()
-        all_vacancies.extend(response["objects"])
-        page: int = 1
-        while response["more"]:
+        page: int = 0  # как избавится от того, чтобы говорить, что страница 0
+        while True:
             response = self.get_response_by_page(page)
             all_vacancies.extend(response["objects"])
+            if not response["more"]:
+                break
             page += 1
         return all_vacancies
 
