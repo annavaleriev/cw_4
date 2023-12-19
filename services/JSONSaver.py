c394f22d9cacde4c21aa6633be42e9f15a5318ef
annavaleriev <filenko.a@gmail.com>
2023-12-19 23:10:21 +0300

    Обновлен класс JSONSaver и методы в нём def json_saver и def json_read, которые отвечают за запись и чтение файлов в формате json

--- services/JSONSaver.py
+++ services/JSONSaver.py
@@ -1,32 +1,62 @@
 import json
 
+from services.api_abc import API
 
-class JSONSaver:
+
+class JSONSaver(API):
     """
     Класс для работы с файлами json
     """
-    def __init__(self, path):
-        self.path = path  # тут путь к файлу...и какой это путь?
-        # Сохранение информации о вакансиях в файл
-        # json_saver = JSONSaver()
-        # json_saver.add_vacancy(vacancy)
-        # json_saver.get_vacancies_by_salary("100 000-150 000 руб.")
-        # json_saver.delete_vacancy(vacancy)
 
-    def json_saver(self, list_vacancy):
+    def json_saver(self, filename):
         """
         Запись в файл вакансий
-        :param list_vacancy:
+        :param filename:
         :return:
         """
+        all_vacancies = self.get_all_vacancies()
 
-        with open(self.path, "w", encoding="utf-8") as file:
-            json.dump(list_vacancy, file, indent=2, ensure_ascii=False)
+        with open(filename, "w", encoding="utf-8") as file:
+            json.dump(all_vacancies, file, indent=2, ensure_ascii=False)
 
-    def json_read(self):
+    @staticmethod
+    def json_read(filename):
         """
         Открывает файл на чтение
         :return:
         """
-        with open(self.path, "r", encoding="utf-8") as file:
-            json.load(file)
+        with open(filename, "r", encoding="utf-8") as file:
+            data = json.load(file)
+        return data
+
+# import json
+#
+#
+# class JSONSaver:
+#     """
+#     Класс для работы с файлами json
+#     """
+#
+#     def __init__(self, test):
+#         self.test = test
+#
+#     def json_saver(self, filename):
+#         """
+#         Запись в файл вакансий
+#         :param filename:
+#         :return:
+#         """
+#         all_vacancies = self.test.get_all_vacancies()
+#
+#         with open(filename, "w", encoding="utf-8") as file:
+#             json.dump(all_vacancies, file, indent=2, ensure_ascii=False)
+#
+#     @staticmethod
+#     def json_read(filename):
+#         """
+#         Открывает файл на чтение
+#         :return:
+#         """
+#         with open(filename, "r", encoding="utf-8") as file:
+#             data = json.load(file)
+#         return data
