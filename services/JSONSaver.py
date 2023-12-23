67c27c2207d713ddf9b3f38b9f3f5e2ee7bb6a11
annavaleriev <filenko.a@gmail.com>
2023-12-23 18:03:40 +0300

    Дописаны docstring и type hints для def json_saver и def json_read

--- services/JSONSaver.py
+++ services/JSONSaver.py
@@ -1,62 +1,27 @@
 import json
 
-from services.api_abc import API
 
-
-class JSONSaver(API):
+class JSONSave:
     """
     Класс для работы с файлами json
     """
 
-    def json_saver(self, filename):
+    def json_saver(self, filename, all_vacancies) -> None:
         """
-        Запись в файл вакансий
-        :param filename:
-        :return:
+        Метод, который записывает в файл вакансии
+        :param filename:                          # забыла что тут
+        :param all_vacancies: список с вакансиями
+        :return: None
         """
-        all_vacancies = self.get_all_vacancies()
-
         with open(filename, "w", encoding="utf-8") as file:
             json.dump(all_vacancies, file, indent=2, ensure_ascii=False)
 
     @staticmethod
-    def json_read(filename):
+    def json_read(filename) -> dict:
         """
         Открывает файл на чтение
-        :return:
+        :return: файл с вакансиями в формсате json
         """
         with open(filename, "r", encoding="utf-8") as file:
             data = json.load(file)
         return data
-
-# import json
-#
-#
-# class JSONSaver:
-#     """
-#     Класс для работы с файлами json
-#     """
-#
-#     def __init__(self, test):
-#         self.test = test
-#
-#     def json_saver(self, filename):
-#         """
-#         Запись в файл вакансий
-#         :param filename:
-#         :return:
-#         """
-#         all_vacancies = self.test.get_all_vacancies()
-#
-#         with open(filename, "w", encoding="utf-8") as file:
-#             json.dump(all_vacancies, file, indent=2, ensure_ascii=False)
-#
-#     @staticmethod
-#     def json_read(filename):
-#         """
-#         Открывает файл на чтение
-#         :return:
-#         """
-#         with open(filename, "r", encoding="utf-8") as file:
-#             data = json.load(file)
-#         return data
