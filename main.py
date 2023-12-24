ee65ecacba9f999a02ff3ae7758b51fd14a2d09b
annavaleriev <filenko.a@gmail.com>
2023-12-24 17:27:31 +0300

    Обновлён def user_interaction для работы пользователя с вакансиями

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from services.JSONSaver import JSONSave
 from services.api_hh import HeadHunterAPI
 from services.api_sj import SuperJobAPI
+from services.vacancy import Vacancy
 
 
 def user_interaction():
@@ -15,19 +16,32 @@ def user_interaction():
 
     print("\nПожалуйста, подождите, мы ищем для вас вакансии.")
 
+    hh_vacancies = []
+    superjob_vacancies = []
+
     if platform in (1, 3):
         hh_api = HeadHunterAPI(keyword)
-        hh_vacancies = hh_api.get_all_vacancies()
+        hh_vacancies.extend(hh_api.get_all_vacancies())
+        # hh_vacancy = Vacancy
+        # hh_exemplars = hh_vacancy.get_exemplars_hh(hh_vacancies)
+        hh_exemplars = Vacancy.get_exemplars_hh(hh_vacancies)
+        for vacancy in hh_exemplars: # идём по списку с экземплярами и выводим
+            print(str(vacancy))
+
     elif platform in (2, 3):
         superjob_api = SuperJobAPI(keyword)
-        superjob_vacancies = superjob_api.get_all_vacancies()
+        superjob_vacancies.extend(superjob_api.get_all_vacancies())
+        sj_exemplars = Vacancy.get_exemplars_sj(superjob_vacancies)
     else:
         print("Вы ввели неверную цифру. Вам нужно выбрать один из вариантов ниже.\n"
               "Выберите сайт, с которого хотите получить вакансии:\n1 - HH\n2 - SJ\n3 - оба сайта")
         return
 
     json_save = JSONSave()
-    json_save.json_saver("jfjfjfj.json", hh_vacancies)
+    # json_save.json_saver("вакансии.json", hh_vacancies)
+    json_save.json_saver("вакансии.json",  hh_exemplars)
 
 
 user_interaction()
+
+# all_exemplars = hh_exemplars + sj_exemplars  а это мне куда засунуть?
