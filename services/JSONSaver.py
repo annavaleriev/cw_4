645839fd661b4e1956d784c749fa7b09478ae38e
annavaleriev <filenko.a@gmail.com>
2024-01-17 19:18:00 +0300

    Обновлен класс class WorkWithJson

--- services/JSONSaver.py
+++ services/JSONSaver.py
@@ -6,33 +6,32 @@ class WorkWithJson:
     """
     Класс для работы с файлами json
     """
+    def __init__(self, filename):
+        self.filename = filename
 
-    @staticmethod
-    def json_saver(filename, all_vacancies) -> None:
+    def json_saver(self, all_vacancies) -> None:
         """
         Метод, который записывает в файл вакансии
         :param filename:                          # забыла что тут
         :param all_vacancies: список с вакансиями
         :return: None
         """
-        with open(filename, "w", encoding="utf-8") as file:
+        with open(self.filename, "w", encoding="utf-8") as file:
             json.dump(all_vacancies, file, indent=2, ensure_ascii=False)
 
-    @staticmethod
-    def json_read(filename) -> dict:
+    def json_read(self) -> dict:
         """
         Открывает файл на чтение
         :return: файл с вакансиями в формсате json
         """
-        with open(filename, "r", encoding="utf-8") as file:
+        with open(self.filename, "r", encoding="utf-8") as file:
             data = json.load(file)
         return data
 
-    @staticmethod
-    def json_delete(filename) -> None:
+    def json_delete(self) -> None:
         """
         Метод, который удаляет файл
         :param filename: название файла
         :return: None
         """
-        os.remove(filename)
+        os.remove(self.filename)
