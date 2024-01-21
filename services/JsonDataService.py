b30bb6e409b3c264d798f1260557bd6c14a56ccf
annavaleriev <filenko.a@gmail.com>
2024-01-21 13:20:20 +0300

    Переименован класс JsonSaver в JsonDataService

--- services/JsonDataService.py
+++ services/JsonDataService.py
@@ -2,27 +2,26 @@ import json
 import os
 
 
-class WorkWithJson:
+class JsonDataService:
     """
     Класс для работы с файлами json
     """
     def __init__(self, filename):
         self.filename = filename
 
-    def json_saver(self, all_vacancies) -> None:
+    def saver(self, all_vacancies) -> None:
         """
         Метод, который записывает в файл вакансии
-        :param filename:                          # забыла что тут
         :param all_vacancies: список с вакансиями
         :return: None
         """
         with open(self.filename, "w", encoding="utf-8") as file:
             json.dump(all_vacancies, file, indent=2, ensure_ascii=False)
 
-    def json_read(self) -> dict:
+    def read(self) -> dict:
         """
         Открывает файл на чтение
-        :return: файл с вакансиями в формсате json
+        :return: файл с вакансиями в формате json
         """
         with open(self.filename, "r", encoding="utf-8") as file:
             data = json.load(file)
@@ -31,7 +30,6 @@ class WorkWithJson:
     def json_delete(self) -> None:
         """
         Метод, который удаляет файл
-        :param filename: название файла
         :return: None
         """
         os.remove(self.filename)
